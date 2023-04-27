1da3090957a595f7b5abf79e3012807625a6db8d
MayconFelipeBianchi <114965402+MayconFelipeBianchi@users.noreply.github.com>
2023-04-26 21:18:41 -0300

    Add files via upload

---
+++ ExercicioSobreContaBancaria/src/main/java/com/mycompany/exerciciosobreempregados/ContaEspecial.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exerciciosobreempregados;
+
+/**
+ *
+ * @author aluno
+ */public class ContaEspecial extends ContaBancaria {
+    private final float limite;
+
+    public ContaEspecial(String cliente, int num_conta, float saldo, float limite) {
+        super(cliente, num_conta, saldo);
+        this.limite = limite;
+    }
+
+    @Override
+    public void sacar(float valor) {
+        if (valor > 0 && (saldo - valor) >= -limite) {
+            saldo -= valor;
+            System.out.println("Saque realizado com sucesso. Novo saldo: R$ " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente para realizar o saque.");
+        }
+    }
+}
---
+++ ExercicioSobreContaBancaria/src/main/java/com/mycompany/exerciciosobreempregados/ExercicioSobreEmpregados.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.exerciciosobreempregados;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExercicioSobreEmpregados {
+
+    public static void main(String[] args) {
+
+        ContaBancaria conta1 = new ContaBancaria("João Silva", 1001, 1500.0f);
+        ContaPoupanca conta2 = new ContaPoupanca("Maria Santos", 2002, 3000.0f, 15);
+        ContaEspecial conta3 = new ContaEspecial("Pedro Souza", 3003, 2000.0f, 1000.0f);
+
+        conta1.sacar(500.0f);
+        conta1.depositar(1000.0f);
+
+        conta2.calcularNovoSaldo(0.5f);
+
+        conta3.sacar(2500.0f);
+        conta3.sacar(1000.0f);
+        conta3.depositar(500.0f);
+
+        System.out.println("Dados da Conta");
+    }
+}
