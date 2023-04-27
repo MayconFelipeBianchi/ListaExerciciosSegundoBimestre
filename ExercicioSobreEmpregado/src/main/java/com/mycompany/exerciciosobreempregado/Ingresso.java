0cef63c11a4148fadcf771dc085e88aec6e899bb
MayconFelipeBianchi <114965402+MayconFelipeBianchi@users.noreply.github.com>
2023-04-26 21:08:44 -0300

    Add files via upload

---
+++ ExercicioSobreEmpregado/src/main/java/com/mycompany/exerciciosobreempregado/Ingresso.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exerciciosobreempregado;
+
+/**
+ *
+ * @author aluno
+ */
+
+public class Ingresso {
+    private double valor;
+
+    public Ingresso(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingresso com valor de R$" + String.format("%.2f", valor);
+    }
+}
+
